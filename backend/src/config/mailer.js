7c376ebaf91718e7052332c3d72fcc18c40f0e98
Garudra <garudra007@gmail.com>
2026-08-09 02:28:31 +0530

    Fix SMTP connection for production

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -2,10 +2,13 @@ import "dotenv/config";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 
